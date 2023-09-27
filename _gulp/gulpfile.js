5950b46d39bda2441079b82eab2a011b94eca9c7
s-tani09 <s.tanigami09@gmail.com>
2023-09-27 23:00:50 +0900

    修正

--- _gulp/gulpfile.js
+++ _gulp/gulpfile.js
@@ -162,9 +162,9 @@ const jsBabel = () => {
 // ブラウザーシンク
 const browserSyncOption = {
   notify: false,
-  // server: "../dist/", // ローカルサーバーのルートディレクトリ
+  server: "../dist/", // ローカルサーバーのルートディレクトリ
   //WordPressの場合は↓を有効にする。その場合、↑(server)はコメントアウトする。
-  proxy: "diving-wordpress.local", // ローカルサーバーのURL（WordPress）
+  // proxy: "http://localhost:8888", // ローカルサーバーのURL（WordPress）
 };
 const browserSyncFunc = () => {
   browserSync.init(browserSyncOption);
